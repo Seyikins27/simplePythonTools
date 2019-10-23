23664674ef9f83688e08e7fecd484a746270c8b6
YOUR_USERNAME <seyiboyvvu@gmail.com>
2019-10-23 19:26:30 +0100

    can list all similar files

--- search.py
+++ search.py
@@ -4,21 +4,24 @@ from difflib import SequenceMatcher
 def is_directory(directory):
     if os.path.isdir(directory):
         return True
-    print ('Not a Directory')
+    print ('Enter a valid Directory')
     exit()
+
+def similar(string, file):
+    return SequenceMatcher(None,string,file).ratio()
 
 def main():
     print('___ This is a simple file search tool in Python ___')
     directory=input('Enter The Directory you want to search through: ')
     is_directory(directory)
     string=input('Enter you search query')
-
+    similar_files=[]
     for r,d,f in os.walk(directory):
         for file in f:
-            print(file)
+            if similar(string,file)>=0.5:
+                similar_files.append(file)
 
-
-    return 0
+    print(similar_files)
 
 if __name__=="__main__":
     main()
