2e5db16807166d1f7ca8116fad8a1c1eaff44029
YOUR_USERNAME <seyiboyvvu@gmail.com>
2019-10-23 20:05:49 +0100

    Specify string match level

--- search.py
+++ search.py
@@ -14,15 +14,26 @@ def list_items(list):
     for item in list:
         print(item)
 
+def search_level(level):
+    if level=='h' or level=='H':
+        return 0.8
+    elif level=='m' or level=='M':
+        return 0.5
+    elif level=='l' or level=='L':
+        return 0.2
+    else:
+        return 0.5
+
 def main():
     print('___ This is a simple file search tool in Python ___')
     directory=input('Enter The Directory you want to search through: ')
     is_directory(directory)
     string=input('Enter you search query')
+    level=input('Specify string match level: L for Low, M for Medium, H for High')
     similar_files=[]
     for r,d,f in os.walk(directory):
         for file in f:
-            if similar(string,file)>=0.5:
+            if similar(string,file)>=search_level(level):
                 similar_files.append(os.path.join(r,file))
     print(len(similar_files)," results found")
     list_items(similar_files)
